a3f35b2e4c2ef56c649323c22533dec1f1b163d8
abelozerov <abelozerov@gmcs.ru>
2018-11-08 18:22:13 +0300

    TableChange

--- BTApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ BTApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -13,14 +13,13 @@ import 'rxjs/add/operator/catch';
 export class FetchDataComponent {
   private http: HttpClient;
   public forecasts: WeatherForecast[];
-  public employeesBase: EmployeeBase;
-  @Inject('BASE_URL') baseUrl: string
+  public employeesBase: EmployeeBase[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts')
       .subscribe(result => { this.forecasts = result; }, error => console.error(error));
-   /* http.get<EmployeeBase>(baseUrl + `api/EmployeeBases/1`)
-      .subscribe(result => { this.employeesBase = result; }, error => console.error(error)); */
+    http.get<EmployeeBase[]>(baseUrl + `api/EmployeeBases`)
+      .subscribe(result => { this.employeesBase = result; }, error => console.error(error)); 
 
   }
 
@@ -40,6 +39,7 @@ interface EmployeeBase {
   EmployeeBaseId: number;
   Code: string;
   Name: string;
+  EmployeeProjectAssigns: string;
 }
 interface WeatherForecast {
   dateFormatted: string;
