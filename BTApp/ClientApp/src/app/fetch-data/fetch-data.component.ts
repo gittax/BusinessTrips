a3bc0f57d0f715f31a9b7856d5ff8ee3661a8dff
abelozerov <abelozerov@gmcs.ru>
2018-11-08 17:21:41 +0300

    Added Server Rendering and Data Transmit support

--- BTApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ BTApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -2,7 +2,9 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
-import { catchError, map, tap } from 'rxjs/operators';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-fetch-data',
@@ -12,19 +14,14 @@ export class FetchDataComponent {
   private http: HttpClient;
   public forecasts: WeatherForecast[];
   public employeesBase: EmployeeBase;
+  @Inject('BASE_URL') baseUrl: string
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-    http.get<EmployeeBase>(baseUrl + `api/EmployeeBases/1`).subscribe(result => {
-      this.employeesBase = result;
-    }, error => console.error(error));
-  }
+    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts')
+      .subscribe(result => { this.forecasts = result; }, error => console.error(error));
+   /* http.get<EmployeeBase>(baseUrl + `api/EmployeeBases/1`)
+      .subscribe(result => { this.employeesBase = result; }, error => console.error(error)); */
 
-  getEmployee(id: number, @Inject('BASE_URL') baseUrl: string){
-    return this.http.get<EmployeeBase[]>(baseUrl + `api/EmployeeBases/${id}`).pipe(
-      catchError(this.handleError<EmployeeBase[]>(`getHero id=${id}`)));
   }
 
   private handleError<T> (operation = 'operation', result?: T) {
