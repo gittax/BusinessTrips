ef059cad60099edb6d7245bb7eb2828b1b27f78b
Alexey Belozerov <abelozerov@GMCS.local>
2018-12-03 16:46:14 +0300

    реализовал CRUD в dev-extreme

--- BTApp/ClientApp/src/app/routes/routes.component.ts
+++ BTApp/ClientApp/src/app/routes/routes.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { HttpService } from '../services/http.service';
-import { Request, Route} from '../models/models';
+import { Request, Route } from '../models/models';
+import DataSource from "devextreme/data/data_source";
 
 @Component({
   selector: 'app-routes',
@@ -13,11 +14,39 @@ export class RoutesComponent implements OnInit {
   private i: number;
   public routes: Route[];
   public requests: Request[];
-  public routeTypes: string[] = ["Flight", "Train"];
-  public ticketTypes: string[] = ["Departure", "Arrival", "Transit"];
-  public classTypes: string[] = ["Economy class", "Premium Economy Class", "Business class", "First class", "Coupe", "Sit"];
-  public flightClassTypes: string[] = ["Economy class", "Premium Economy Class", "Business class", "First class"];
-  public trainClassTypes: string[] = ["Coupe", "Sit"];
+  public routeTypes = [
+    { "Name": "Flight", "ID": "0" },
+    { "Name": "Train", "ID": "1" }
+  ];
+
+  public ticketTypes = [
+    { text: "Departure", value: "0" },
+    { text: "Arrival", value: "1" },
+    { text: "Transit", value: "2" }
+  ];
+
+  public classTypes = [
+    { text: "Economy class", value: "0" },
+    { text: "Premium Economy Class", value: "1" },
+    { text: "Business class", value: "2" },
+    { text: "First class", value: "3" },
+    { text: "Coupe", value: "4" },
+    { text: "Sit", value: "5" }
+  ];
+
+  public flightClassTypes = [
+    { text: "Economy class", value: "0" },
+    { text: "Premium Economy Class", value: "1" },
+    { text: "Business class", value: "2" },
+    { text: "First class", value: "3" }
+  ];
+
+  public trainClassTypes = [
+    { text: "Coupe", value: "4" },
+    { text: "Sit", value: "5" }
+  ];
+
+  dataSource: DataSource;
 
   done: boolean = false;
 
@@ -25,14 +54,13 @@ export class RoutesComponent implements OnInit {
 
   newRoute: Route = new Route();
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   items: Route[] = this.routeViewModels;
 
   pageOfItems: Route[];
 
   ngOnInit() {
-
     this.i = 0;
     this.newRoute = new Route();
 
@@ -41,6 +69,27 @@ export class RoutesComponent implements OnInit {
 
     this.httpService.getRoute()
       .subscribe(result => { this.routes = result; }, error => console.error(error), () => this.callback(this.routes));
+
+  }
+
+  onEditorPreparing(e) {
+    e.editorOptions.disabled = e.parentType === "dataRow" && e.dataField === "routeId"
+  };
+
+  onRowInserted(e) {
+    this.addRoute(e.data);
+  }
+
+  onRowUpdated(e) {
+    console.log(e.data);
+    var rt: Route = this.routes.find(x => x.routeId == e.key)
+    var data = Object.assign({}, rt, e.data)
+    console.log(data);
+    this.editRoute(e.key, data);
+  }
+
+  onRowRemoved(e) {
+    this.deleteRoute(e.key);
   }
 
   addRoute(newRoute: Route) {
@@ -51,10 +100,10 @@ export class RoutesComponent implements OnInit {
   editRoute(id: string, route: Route) {
     this.httpService.putRoute(id, route)
       .subscribe(result => { console.log(result) }, error => console.error(error), () => this.ngOnInit());
-    var editButton = document.getElementsByClassName('edit');
+    /*var editButton = document.getElementsByClassName('edit');
     var addButton = document.getElementsByClassName('add');
     addButton[0].removeAttribute('hidden');
-    editButton[0].setAttribute('hidden', '');
+    editButton[0].setAttribute('hidden', '');*/
   }
 
   deleteRoute(id: string) {
@@ -83,15 +132,26 @@ export class RoutesComponent implements OnInit {
       this.routeViewModels[i].arrivalCity = rts.arrivalCity;
       this.routeViewModels[i].arrivalTime = rts.arrivalTime;
       this.routeViewModels[i].budget = rts.budget;
-      this.routeViewModels[i].classType = this.classTypes[rts.classType];
+      this.routeViewModels[i].classType = rts.classType;
       this.routeViewModels[i].departureCity = rts.departureCity;
       this.routeViewModels[i].departureTime = rts.departureTime;
       this.routeViewModels[i].flightNumber = rts.flightNumber;
       this.routeViewModels[i].requestId = rts.requestId;
       this.routeViewModels[i].routeId = rts.routeId;
-      this.routeViewModels[i].routeType = this.routeTypes[rts.routeType];
-      this.routeViewModels[i].ticketType = this.ticketTypes[rts.ticketType];
+      this.routeViewModels[i].routeType = rts.routeType;
+      this.routeViewModels[i].ticketType = rts.ticketType;
     })
+
+
+    /*this.dataSource = new DataSource({
+      store: {
+        type: 'array',
+        data: this.routeViewModels,
+        key: 'routeId'
+      }
+    });
+
+    console.log(this.dataSource);*/
   }
 
   goEdit(id: string) {
